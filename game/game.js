f179e251c521730b29bc1d4b29b9a782d339e993
TheDarkUndoing <38772798+TheDarkUndoing@users.noreply.github.com>
2020-05-17 17:38:46 -0400

    making game

---
+++ game/game.js
@@ -0,0 +1,27 @@
+var myGameArea = {
+  canvas : document.createElement("canvas"),
+  start : function() {
+    this.canvas.width = 480;
+    this.canvas.height = 270;
+    this.context = this.canvas.getContext("2d");
+    document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+  }
+}
+
+function component(width, height, color, x, y) {
+  this.width = width;
+  this.height = height;
+  this.x = x;
+  this.y = y;
+  ctx = myGameArea.context;
+  ctx.fillStyle = color;
+  ctx.fillRect(this.x, this.y, this.width, this.height);
+}
+
+function startGame()
+{
+  myGameArea.start();
+  var myGamePiece;
+  myGamePiece = new component(30, 30, "blue", 10, 120);
+  console.log("worked");
+}
