f700008843a8e85a5877d7c1ced88ff9f80e6245
TheDarkUndoing <38772798+TheDarkUndoing@users.noreply.github.com>
2020-11-29 13:49:38 -0500

    Modification

--- passwordDB/search.js
+++ passwordDB/search.js
@@ -24,25 +24,36 @@ async function searchQuery()
   con.getConnection( async (err,connection) =>
   {
     if (err) throw err;
-    con.query("SELECT passwords FROM password_by_user WHERE username='"+query+"'", async (err, result) =>
+    con.query("SELECT count FROM password_count_by_user WHERE username='"+query+"'", async (err, result) =>
     {
+      console.log(result);
       if ( result[0] != undefined)
       {
-        var passwordList = result[0].passwords.split(",");
+        var count = result[0].count;
         //console.log(passwordList);
-        for(var password in passwordList)
-        {
-          //console.log(passwordList[password]);
-          var list = document.getElementById('result');
-          console.log(list);
-          var entry = document.createElement('li');
-          console.log(entry);
-          entry.classList.add("collection-item");
-          entry.appendChild(document.createTextNode(passwordList[password]));
-          list.appendChild(entry);
-          //document.getElementById("result").innerHTML = result[0].passwords;
-          console.log(passwordList[password]);
-        }
+        var list_item = document.getElementById('result-item');
+
+        list_item.hidden = false
+        list_item.textContent = count+" passwords found for "+query;
+        // var entry = document.createElement('li');
+        // entry.classList.add("collection-item");
+        // entry.appendChild(document.createTextNode(count+" passwords found for "+query));
+        // list.appendChild(entry);
+        // console.log(count);
+        //
+        // for(var password in passwordList)
+        // {
+        //   //console.log(passwordList[password]);
+        //   var list = document.getElementById('result');
+        //   console.log(list);
+        //   var entry = document.createElement('li');
+        //   console.log(entry);
+        //   entry.classList.add("collection-item");
+        //   entry.appendChild(document.createTextNode(passwordList[password]));
+        //   list.appendChild(entry);
+        //   //document.getElementById("result").innerHTML = result[0].passwords;
+        //   console.log(passwordList[password]);
+        // }
 
         //console.log(result[0])
         //console.log("searchQuery ran...")
